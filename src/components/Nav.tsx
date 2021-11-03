ab7cbc1157bdc891e1a9c241d39b32829afe508b
Cyer <1678447359@qq.com>
2021-11-03 15:37:59 +0800

    引入导航栏 icon

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,6 +1,8 @@
 import styled from 'styled-components';
 import {Link} from 'react-router-dom';
 import React from 'react';
+require('icons/detail.svg');
+require('icons/chart.svg');
 
 const NavWrapper = styled.nav`
   box-shadow: 0 0 3px rgba(0, 0, 0, 0.2);
@@ -13,8 +15,12 @@ const NavWrapper = styled.nav`
       width: 33.3333%;
       text-align: center;
       font-size: 12px;
-      padding: 14px 0;
-      &.xxx{
+      padding: 4px 0;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      flex-direction: column;
+      &.money{
         background: #FF983B;
         color: white;
         font-size: 18px;
@@ -29,12 +35,20 @@ const Nav = () => {
     <NavWrapper>
       <ul>
         <li>
-          <Link to="/detail">明细</Link>
+          <svg className="icon">
+            <use xlinkHref="#detail"/>
+          </svg>
+          <Link to="/detail">
+            明细
+          </Link>
         </li>
-        <li className="xxx">
+        <li className="money">
           <Link to="/money">记一笔</Link>
         </li>
         <li>
+          <svg className="icon">
+            <use xlinkHref="#chart"/>
+          </svg>
           <Link to="/chart">图表</Link>
         </li>
       </ul>
