44c5badea8ed144d9f23025dbeeaa11ba2b6e0f8
Cyer <1678447359@qq.com>
2021-11-03 21:43:22 +0800

    LabelSection 组件添加 svg

--- src/views/Money/LabelSection.tsx
+++ src/views/Money/LabelSection.tsx
@@ -18,19 +18,57 @@ const Wrapper = styled.section`
 
   > .content {
     overflow: auto;
+    height: 227px;     //待定
 
     > ul {
       display: flex;
       flex-wrap: wrap;
       padding: 12px 0;
-      border: 1px solid red;
-
+      
       > li {
         width: 33.33333%;
         height: 85px;
         display: flex;
         justify-content: center;
         align-items: center;
+        > div{
+          display: flex;
+          justify-content: center;
+          align-items: center;
+          flex-direction: column;
+          font-size: 16px;
+          width: 90%;
+          height: 75px;
+          border-radius: 20px;
+          &.selected {
+            background: lavenderblush;
+
+            svg {
+              animation: shake 0.3s linear;
+            }
+          }
+          >.icon{
+            width: 45px;
+            height: 45px;
+          }
+        }
+        @keyframes shake {
+          0% {
+            transform: rotate(0deg);
+          }
+          20% {
+            transform: rotate(20deg);
+          }
+          40% {
+            transform: rotate(0deg);
+          }
+          80% {
+            transform: rotate(-20deg);
+          }
+          100% {
+            transform: rotate(0deg);
+          }
+        }
       }
     }
   }
@@ -44,10 +82,48 @@ const LabelSection = () => {
       </div>
       <div className="content">
         <ul>
-          <li>图1</li>
-          <li>图2</li>
-          <li>图3</li>
-          <li>图4</li>
+          <li>
+            <div className="selected">
+              <Icon name="三餐"/>
+              <span>三餐</span>
+            </div>
+          </li>
+          <li>
+            <div>
+              <Icon name="衣服"/>
+              <span>衣服</span>
+            </div>
+          </li>
+          <li>
+            <div>
+              <Icon name="宠物"/>
+              <span>宠物</span>
+            </div>
+          </li>
+          <li>
+            <div>
+              <Icon name="医疗"/>
+              <span>医疗</span>
+            </div>
+          </li>
+          <li>
+            <div>
+              <Icon name="零食"/>
+              <span>零食</span>
+            </div>
+          </li>
+          <li>
+            <div>
+              <Icon name="学习"/>
+              <span>学习</span>
+            </div>
+          </li>
+          <li>
+            <div>
+              <Icon name="设置"/>
+              <span>设置</span>
+            </div>
+          </li>
         </ul>
       </div>
     </Wrapper>
