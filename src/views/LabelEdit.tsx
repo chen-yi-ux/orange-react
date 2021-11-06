0709a8febcd1b619da832cf9adc422463d4fb398
Cyer <1678447359@qq.com>
2021-11-06 21:41:39 +0800

    编辑标签页面的点击类型匹配

--- src/views/LabelEdit.tsx
+++ src/views/LabelEdit.tsx
@@ -1,8 +1,9 @@
 import {Icon} from 'components/Icon';
 import styled from 'styled-components';
-import React from 'react';
+import React, {useState} from 'react';
 import {useLabel} from 'hooks/useLabel';
-import { Link } from 'react-router-dom';
+import {Link} from 'react-router-dom';
+import classnames from 'classnames';
 
 const Wrapper = styled.div`
   height: 100vh;
@@ -13,7 +14,7 @@ const Header = styled.div`
   padding-top: 15px;
   padding-left: 10px;
   padding-right: 10px;
-  
+
   > a > .icon {
     width: 24px;
     height: 24px;
@@ -53,8 +54,10 @@ const Header = styled.div`
 const Main = styled.div`
   height: calc(100% - 185px);
   overflow: auto;
+
   > ul {
     overflow: auto;
+
     > li {
       display: flex;
       justify-content: space-between;
@@ -62,18 +65,21 @@ const Main = styled.div`
       height: 45px;
       padding: 0 15px;
       border-bottom: 1px solid #e3e3e3;
-      > .content{
+
+      > .content {
         display: flex;
         justify-content: space-between;
         align-items: center;
         font-size: 16px;
-        > .icon{
+
+        > .icon {
           margin-right: 8px;
           width: 30px;
           height: 30px;
         }
       }
-      > .icon{
+
+      > .icon {
         margin-top: 8px;
         width: 20px;
         height: 20px;
@@ -87,30 +93,33 @@ const Button = styled.div`
   align-items: center;
   background: white;
   font-size: 18px;
-  box-shadow: 0 0 3px rgba(0,0,0,0.2);
+  box-shadow: 0 0 3px rgba(0, 0, 0, 0.2);
   height: 75px;
+
   > a {
     display: flex;
     justify-content: center;
     align-items: center;
     flex-direction: column;
-    > .icon{
+
+    > .icon {
       width: 23px;
       height: 23px;
     }
-    > span{
+
+    > span {
       color: black;
     }
   }
-  
+
 `;
 
+type Category = '-' | '+'
 
 const LabelEdit: React.FC = () => {
   const {labels, deleteLabel} = useLabel();
-  // const categoryList = useState<('-'|'+')[]>(['+', '-']);
-  // const categoryMap = {'-': '支出', '+': '收入'};
-
+  const [category, setCategory] = useState<Category>('-');
+  const labelBlock = labels.filter(item => item.category === category)
   return (
     <Wrapper>
       <Header>
@@ -118,13 +127,17 @@ const LabelEdit: React.FC = () => {
           <Icon name="left-white"/>
         </Link>
         <ul>
-          <li className="income">收入</li>
-          <li className="expenses selected">支出</li>
+          <li className={classnames('income', category === '+' ? 'selected' : '')}
+              onClick={() => {setCategory('+');}}>收入
+          </li>
+          <li className={classnames('expenses', category === '-' ? 'selected' : '')}
+              onClick={() => {setCategory('-');}}>支出
+          </li>
         </ul>
       </Header>
       <Main>
         <ul>
-          {labels.map(item =>
+          {labelBlock.map(item =>
             <li key={item.name}>
               <div className="content">
                 <Icon name={item.svg}/>
